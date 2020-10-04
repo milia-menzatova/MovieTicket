895a337cf9eb7add10e7a3a2453cb507741dfa7e
Milia Anifieieva <milia.menzatova@gmail.com>
2020-10-03 22:08:16 -0400

    Fixed formatting

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@ namespace MovieTicket
     class Program
     {
         static void Main(string[] args)
-        { 
-            int age = 30; 
+        {
+            int age = 30;
             float price = 0.0f;
 
-            if (age >= 0 && age <= 5) 
+            if (age >= 0 && age <= 5)
             {
-              price = 0.0f;  
-            } 
+                price = 0.0f;
+            }
             else if (age > 5 && age <= 14)
             {
                 price = 7.99f;
@@ -23,10 +23,10 @@ namespace MovieTicket
             }
             else if (age > 65)
             {
-              price = 9.99f;  
+                price = 9.99f;
             }
-             Console.WriteLine("Price is " + price);
-            
+            Console.WriteLine("Price is " + price);
+
         }
     }
 }
